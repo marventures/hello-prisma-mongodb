a12c47e9b6d6ad5c315234cb16554c871aab691d
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-10-02 01:34:38 +0800

    feat: initialize project

---
+++ index.ts
@@ -0,0 +1,20 @@
+// REFERENCE: https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/mongodb-typescript-mongodb
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
